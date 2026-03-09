7e8bbe458f5c7a11a4cc550fd17ccc603b4792f6
VAARchar 234 <vaarchar234@gmail.com>
2026-03-09 12:32:05 +0800

    New Folder

---
+++ new_folder/newfolder.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function newfolder() {
+  return (
+    <View>
+      <Text>newfolder</Text>
+    </View>
+  )
+}
